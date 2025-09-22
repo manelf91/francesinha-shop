61a3b670516fafec967b2cf0bff36fbf16c80628
manelf91 <manuelssgomesferreira@gmail.com>
2025-09-22 08:53:38 +0100

    monolith removed

--- backend/src/main/java/shop/francesinha/backend/repo/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package shop.francesinha.backend.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import shop.francesinha.backend.model.Review;
-
-public interface ReviewRepository extends MongoRepository<Review, String> {
-
-}
